c258240c891faf8f542ea36a5a09dee1ac1786ad
malenec <90346924+malenec@users.noreply.github.com>
2022-09-05 13:27:56 +0200

    laver udskriv

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Node node0 = new Node("halløj");
+        Node node1 = new Node("hej");
+        Node node2 = new Node("med");
+        Node node3 = new Node("dig");
+
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+        node3.previous = node2;
+        node2.previous = node1;
+        node1.previous = node0;
+
+
+
+        Node n = node0;
+
+        printFromHead(n);
+
+    }
+
+    private static void printFromHead(Node n) {
+        while(n != null){
+
+            System.out.println(n.data);
+            n = n.next;
+        }
     }
 
 }
